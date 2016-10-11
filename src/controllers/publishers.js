746f3849bc5e7133d3af8f15e01cca44e19c00b9
Marshall Rose <mrose17@gmail.com>
2016-10-10 19:28:47 -0700

    check for non-existent balance

--- src/controllers/publishers.js
+++ src/controllers/publishers.js
@@ -98,7 +98,7 @@ v1.getBalance =
         }
       }
     ])
-    satoshis = summary[0].satoshis
+    satoshis = summary.length > 0 ? summary[0].satoshis : 0
 
     rate = runtime.wallet.rates[currency.toUpperCase()]
     if (rate) amount = Math.round((satoshis * rate) / 1e8)
