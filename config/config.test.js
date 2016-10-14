e8e6f5ab887eb5848ec02032ec5a08362aa27d0c
Marshall Rose <mrose17@gmail.com>
2016-10-13 23:58:26 -0700

    add username/icon_emoji to config templates

--- config/config.test.js
+++ config/config.test.js
@@ -12,6 +12,8 @@ module.exports =
 , slack             :
   { webhook         : process.env.SLACK_WEBHOOK
   , channel         : process.env.SLACK_CHANNEL
+  , username        : process.env.SLACK_USERNAME
+  , icon_emoji      : process.env.SLACK_ICON_EMOJI
   , icon_url        : process.env.SLACK_ICON_URL
   }
 , login             :
