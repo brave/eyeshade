411e3292d572ca46009a4b6ab94da5b4f68b4660
Marshall Rose <mrose17@gmail.com>
2016-11-04 11:23:07 -0700

    more fields

--- src/controllers/reports.js
+++ src/controllers/reports.js
@@ -117,7 +117,12 @@ v1.publishers.status =
       var datum = await publishers.findOne({ publisher: publisher })
 
       debug('status', datum || 'nil')
-      if (datum) results[publisher] = underscore.extend(results[publisher], underscore.pick(datum, [ 'address', 'authorized' ]))
+      if (!datum) return
+
+      datum.created = new Date(parseInt(datum._id.substring(0, 8), 16) * 1000)
+      datum.modified = new Date((datum.timestamp.high_ * 1000) + datum.timestamp._low_).toISOString()
+      results[publisher] = underscore.extend(results[publisher], underscore.pick(datum, [ '_id', 'publisher', 'timestamp' ]))
+      debug('status', results[publisher])
     })
 
     if (format !== 'csv') return reply(results)
